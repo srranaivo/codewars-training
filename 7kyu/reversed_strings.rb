fc51b128f0146a64b1b4bc4093b4ff95eb217e6c
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-11-22 18:45:58 +0100

    reverse string

---
+++ 7kyu/reversed_strings.rb
@@ -0,0 +1,10 @@
+# Description:
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# Solution:
+def solution(str)
+  str.reverse
+end
