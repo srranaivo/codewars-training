ca70576dbde14e0d958ef39440e3caa6566e1f26
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-11-29 21:49:46 +0100

    string ends with

---
+++ 7kyu/string_ends_with.rb
@@ -0,0 +1,13 @@
+# Deescription:
+# Complete the solution so that it returns true if the first argument(string)
+# passed in ends with the 2nd argument (also a string).
+
+# Examples:
+
+# solution('abc', 'bc') // returns true
+# solution('abc', 'd') // returns false
+
+# My code:
+def solution(str, ending)
+  str.end_with?(ending)
+end
