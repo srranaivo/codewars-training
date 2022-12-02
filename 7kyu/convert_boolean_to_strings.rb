fc065586a0506d83ff37b53301eeb8cc033fc8a9
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-12-02 21:53:02 +0100

    concert boolean to strings

---
+++ 7kyu/convert_boolean_to_strings.rb
@@ -0,0 +1,8 @@
+# Description:
+# Complete the method that takes a boolean value
+# and return a "Yes" string for true, or a "No" string for false.
+
+# My code:
+def bool_to_word bool
+  bool ? 'Yes' : 'No'
+end
