52b4045a0e834d08315f2ee046d24a55446f8f6d
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-12-05 22:07:09 +0100

    using to_s method

---
+++ 7kyu/convert_number_to_string.rb
@@ -0,0 +1,15 @@
+# Description:
+# We need a function that can transform a number (integer) into a string.
+
+# What ways of achieving this do you know?
+# Examples (input --> output):
+
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+# My code:
+def number_to_string(num)
+  # Convert a Number to a String
+  num.to_s
+end
