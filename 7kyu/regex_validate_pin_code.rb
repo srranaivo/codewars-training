d11b1c04e22b26e9f5f580b7478353ce02b8b43b
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-12-16 18:17:22 +0100

    final code with regex

--- 7kyu/regex_validate_pin_code.rb
+++ 7kyu/regex_validate_pin_code.rb
@@ -9,21 +9,64 @@
 # "1234"   -->  true
 # "12345"  -->  false
 # "a234"   -->  false
+
+##########FAUX##########################################
+# def validate_pin(pin)
+#   #return true or false
+#   # le pin ne doit contenir
+#   # que des digits
+#   # les digits ne peuvent être que
+#   # au nombre de 4
+#   # OU au nombre de 6
+#   pin.match?(/^\d{4}$|^\d{6}$/).to_s ? true : false
+# end
+
+# rgex = /^(\d{4}|\d{6})$/
+#   if rgex && (pin.length === 4 || pin.length === 6)
+#     return true
+#   else
+#     return false
+#   end
+
+#   /^(\d{4}|\d{6})$/
+#######################################################33
+
+# My code:
 def validate_pin(pin)
   #return true or false
-  # le pin ne doit contenir
+  # le pin est une string
+  # qui ne doit contenir
   # que des digits
   # les digits ne peuvent être que
   # au nombre de 4
   # OU au nombre de 6
-  pin.match?(/^\d{4}$|^\d{6}$/).to_s ? true : false
-end
-
-rgex = /^(\d{4}|\d{6})$/
-  if rgex && (pin.length === 4 || pin.length === 6)
+#pin.match?(/"[\d]{4}"|"[\d]{6}"/)? ? true : false
+#end
+  #rgex = /^(?:\d{4}|\d{6})$/
+  #if rgex.match(pin)
+# return true
+  #else
+# return false
+  #end
+  checkLength = pin.length === 4 || pin.length === 6
+  numMatch = /^(?:\d{4}|\d{6})$/
+  if (checkLength && pin.match(numMatch))
     return true
   else
     return false
   end
+end
 
-  /^(\d{4}|\d{6})$/
+# Best practice:
+def validate_pin(pin)
+  pin.match? /\A\d{4}(\d{2})?\z/
+end
+
+# Other practice:
+def validate_pin(pin)
+  (pin.length == 4 || pin.length == 6) && pin.count("0-9") == pin.length
+end
+
+def validate_pin(pin)
+  /\A(\d{4}|\d{6})\z/ === pin
+end
