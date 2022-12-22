6e92c9abe93bf61b972bb6735e03537f5dbc10ec
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-12-22 18:03:46 +0100

    solution finale

--- 6kyu/bouncing_balls.rb
+++ 6kyu/bouncing_balls.rb
@@ -28,10 +28,44 @@
 # My code:
 def bouncingBall(h, bounce, window)
   # your code
-return -1 if (h <= 0) || ( bounce <= 0) || (bounce >= 1) || (window >= h)
-counter = 0
-while h >= window
-  (h *= bounce) > window ? counter += 2 : counter += 1
+  # si les conditions ne sont pas remplies
+  # le code renvoie -1
+  return -1 if (h <= 0) || ( bounce <= 0) || (bounce >= 1) || (window >= h)
+  # si les 3 conditions sont remplies
+  # le code doit renvoyer le nombre de fois que la mère
+  # voit descendre et
+  # remonter le ballon
+  bounce_heights = [h]
+  while bounce_heights.last > window
+    bounce_heights << bounce_heights.last * bounce
+  end
+  (bounce_heights.size-1) * 2 - 1
 end
-counter
+
+# Best pratice:
+def bouncingBall(h, bounce, window)
+  bounce < 0 || bounce >= 1 || h <= window || window < 0 ? -1 : bouncingBall(h * bounce, bounce, window) + 2
+end
+
+# Other solutions:
+
+def bouncingBall(h, bounce, window)
+  if h <= 0 || bounce < 0 || bounce >= 1 || window >= h
+    -1
+  elsif h * bounce <= window
+    1
+  else
+    return 2 + bouncingBall(h * bounce, bounce, window)
+  end
+end
+
+def bouncingBall(h, bounce, window)
+  return -1 unless h > 0 && h > window && bounce > 0 && bounce < 1
+  count = 1
+  h *= bounce
+  while h > window
+    count += 2
+    h *= bounce
+  end
+  count
 end
