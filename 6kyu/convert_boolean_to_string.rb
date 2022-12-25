961d1943464d2e6b59fdd0db528cccc16475b40f
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-12-25 17:38:56 +0100

    convert boolean to string

---
+++ 6kyu/convert_boolean_to_string.rb
@@ -0,0 +1,19 @@
+# Description:
+# Implement a function which convert the given boolean value
+# into its string representation.
+
+# Note: Only valid inputs will be given.
+
+# My code:
+def boolean_to_string(b)
+  return b.to_s
+end
+
+# Others solutions:
+def boolean_to_string(b)
+  return b.to_s
+end
+
+def boolean_to_string(b)
+  b.inspect
+end
