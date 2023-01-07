e795bad69a12c11e6ac8d0aea3f3f8550240cb3b
sophie ramahenina <ramahenina.sophie@gmail.com>
2023-01-07 22:39:57 +0100

    solution finale avec des conditions

--- 6kyu/who_likes_it.rb
+++ 6kyu/who_likes_it.rb
@@ -13,3 +13,38 @@
 # ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 
 # Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+# My code:
+def likes(names)
+  #your code here
+  total = names.count
+  if names == []
+    return 'no one likes this'
+  elsif total == 1
+    return "#{names[0]} likes this"
+  elsif total == 2
+    return "#{names[0]} and #{names[1]} like this"
+  elsif total == 3
+    return "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  elsif total > 3
+    return "#{names[0]}, #{names[1]} and #{total - 2} others like this"
+  else
+    return "try again"
+  end
+end
+
+# Best practice
+def likes(names)
+  case names.size
+  when 0
+    "no one likes this"
+  when 1
+    "#{names[0]} likes this"
+  when 2
+    "#{names[0]} and #{names[1]} like this"
+  when 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+    "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+  end
+end
